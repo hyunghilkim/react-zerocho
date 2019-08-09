91ed967fc28b56d58b6f164cc741a4fd7d3f0520
gusrlf14 <gusrlf14@gmail.com>
2019-08-09 17:47:13 +0900

    웹팩 설정하기

--- basic/lecture/webpack.config.js
+++ basic/lecture/webpack.config.js
@@ -4,10 +4,18 @@ module.exports = {
   name: "word-relay-setting",
   mode: "development", //실서비스 : production
   devtool: "eval", // 빠르게 한다.
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
   // 입력
-  entry: {},
+  entry: {
+    // 이미 다른곳에서 불러왔으면 그 파일은 넣을 필요가 없다.
+    app: ["./client"]
+  },
   //출력
   output: {
-    path: path.join(__dirname, "dist")
+    //C:\users\김현길\Desktop\local\react-zerocho\basic\lectture\dist  * __dirname : 현재폴더
+    path: path.join(__dirname, "dist"),
+    filename: "app.js"
   }
 };
